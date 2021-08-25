78a72b8272fd8f64fa8605e35f5af1ed33d3f96b
kseniia_makarova <kseniia.makarova.kyiv@gmail.com>
2021-08-25 11:44:02 +0300

    added task description

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
