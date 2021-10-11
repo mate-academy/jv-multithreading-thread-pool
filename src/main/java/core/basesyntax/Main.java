b9d498b8c75c8cd93bbe3a7e08801fdc8ad65279
Kostia Dementiev <konstde00@gmail.com>
2021-10-12 00:10:24 +0300

    fixed travis

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,8 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
-
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        Callable<String> myThread = new MyThread();
+        MyThread myThread = new MyThread();
         for (int i = 0; i < 20; i++) {
             futures.add(executorService.submit(myThread));
         }
