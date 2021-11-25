f7ef78e455dc99966216a1745f948e97aa5bfdca
Yehor Zapara <egor.zapara1994@gmail.com>
2021-11-25 21:52:56 +0200

    Logs was created

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = new Random().nextInt(900);
+        Thread.sleep(duration);
+        return String.format(
+                "Task duration was %3s ms, execution finished at %.15s",
+                duration, LocalTime.now());
     }
 }
