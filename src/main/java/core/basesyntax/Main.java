0ca5652ccf489c75fd0c0b79a50dddc6408449ea
Roman Kozhevnikow <roman.kozhevnikow@microfocus.com>
2021-12-09 23:23:26 +0200

    solution was added

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +12,20 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        List<Callable<String>> callables = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            callables.add(new MyThread());
+        }
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        try {
+            futures = executorService.invokeAll(callables);
+            executorService.shutdown();
+            for (Future<String> future : futures) {
+                logger.info(future.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
