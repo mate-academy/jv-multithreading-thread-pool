c20058797d40b4d4bdb4c9a83d7f75b70ba704b8
Roman Kozhevnikow <roman.kozhevnikow@microfocus.com>
2021-12-09 23:28:38 +0200

    fixed checkstyle

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,9 +2,11 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
-import java.util.stream.Collectors;
-
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,7 +18,7 @@ public class Main {
         for (int i = 0; i < 20; i++) {
             callables.add(new MyThread());
         }
-        List<Future<String>> futures = new ArrayList<>();
+        List<Future<String>> futures;
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         try {
             futures = executorService.invokeAll(callables);
@@ -25,7 +27,7 @@ public class Main {
                 logger.info(future.get());
             }
         } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Something was went wrong..", e);
         }
     }
 }
