e9509912804945b38ab259482e5d40dd883f696c
Andriy-dz <andriydzdz@gmail.com>
2022-01-04 11:40:26 +0200

    implemented MyThread and Main classes

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime start = LocalTime.now();
+        Thread.sleep(5);
+        LocalTime end = LocalTime.now();
+        return "Task duration was " + (end.getNano() - start.getNano())
+                + " ms, execution finished at " + end;
     }
 }
