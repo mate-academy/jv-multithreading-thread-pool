61a51eb865ab5c112354df7eb29fdb0bbb51b7e8
Andriy-dz <andriydzdz@gmail.com>
2022-01-10 10:01:29 +0200

    create new varible

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,15 +16,16 @@ public class Main {
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
         ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> myThread = new MyThread();
         for (int i = 0; i < 20; i++) {
-            futures.add(executorService.submit(new MyThread()));
+            futures.add(executorService.submit(myThread));
         }
         executorService.shutdown();
         for (Future<String> future : futures) {
             try {
                 logger.info(future.get());
             } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException("something went wrong");
+                throw new RuntimeException("something went wrong", e);
             }
         }
     }
