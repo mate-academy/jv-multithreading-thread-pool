bed71b3df149870e524758e4eff84c20f86c2eb9
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2022-01-10 10:13:09 +0200

    magic number was removed, constant was added

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -5,9 +5,11 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int MAX_DURATION = 100;
+
     @Override
     public String call() throws InterruptedException {
-        int duration = new Random().nextInt(100);
+        int duration = new Random().nextInt(MAX_DURATION);
         Thread.sleep(duration);
         return String.format("Task duration was %s ms, "
                 + "execution finished at %s", duration, LocalTime.now());
