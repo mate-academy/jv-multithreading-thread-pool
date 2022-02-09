d32e0bdc895c8c6657980d16b05d48fe119afb5e
Dany Nesterov <nesterov1200997@gmail.com>
2022-02-09 18:26:59 +0200

    task was completed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +14,19 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> task = new MyThread();
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        for (int i = 0; i <= 5; i++) {
+            futures.add(executorService.submit(task));
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException("Something went wrong");
+            }
+        }
     }
 }
