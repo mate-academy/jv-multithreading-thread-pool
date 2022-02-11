bb8e2badf39680e94394a875fda70d874e617ba0
Olha <FreakyStyley85@gmail.com>
2022-02-11 14:45:46 +0200

    changed method main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -11,12 +12,14 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int SIZE_THREAD_POOL = 5;
+    private static final int SIZE_FUTURES = 20;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-        MyThread task = new MyThread();
-        for (int i = 0; i < 20; i++) {
+        ExecutorService executor = Executors.newFixedThreadPool(SIZE_THREAD_POOL);
+        Callable<String> task = new MyThread();
+        while (futures.size() != SIZE_FUTURES) {
             futures.add(executor.submit(task));
         }
         executor.shutdown();
