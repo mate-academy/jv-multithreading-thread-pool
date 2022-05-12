69c500a05352eaf35d91e3fb22250628e2d119aa
sviat-h <svyatoslav.grinik@gmail.com>
2022-05-12 15:27:29 +0300

    thread pool was implemented

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +13,18 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        List<Future<String>> futureList = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            futureList.add(executorService.submit(new MyThread()));
+        }
+        executorService.shutdown();
+        for (Future<String> future : futureList) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("An exception occurred.", e);
+            }
+        }
     }
 }
