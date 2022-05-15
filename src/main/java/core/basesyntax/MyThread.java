726df7bec8a78316663d0ca0900ab0294ac0f64c
Viktoriia <viksonchik1996@ukr.net>
2022-05-15 21:22:32 +0300

    -call method was implemented -log file was added -the condition is fulfilled in the main() method

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int randomValue = new Random()
+                .ints(1, 6)
+                .findFirst()
+                .getAsInt() * 100;
+        Thread.sleep(randomValue);
+        return "Task duration was " + randomValue + " ms, execution finished at " + LocalTime.now();
     }
 }
