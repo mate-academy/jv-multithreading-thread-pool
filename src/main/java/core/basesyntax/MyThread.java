3c15bcc0d81c0cc79a7e62a141ed5f9ae5879674
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-05-27 12:34:37 +0300

    mvn clean package

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -9,18 +9,18 @@ public class MyThread implements Callable<String> {
     private SecureRandom random;
 
     public MyThread() {
-            try {
-                this.random = SecureRandom.getInstanceStrong();
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException("Exception during initialization 'Random'.", e);
-            }
+        try {
+            this.random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Exception during initialization 'Random'.", e);
         }
+    }
 
-        @Override
-        public String call() throws InterruptedException {
-            int durationTime = random.nextInt(200);
-            Thread.sleep(durationTime);
-            return "Task duration was " + random.nextInt(200)
-                    + " ms, execution finished at " + LocalTime.now();
-        }
+    @Override
+    public String call() throws InterruptedException {
+        int durationTime = random.nextInt(200);
+        Thread.sleep(durationTime);
+        return "Task duration was " + random.nextInt(200)
+                + " ms, execution finished at " + LocalTime.now();
+    }
 }
