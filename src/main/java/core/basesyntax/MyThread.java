f6dd224f1256b9fe0168ea217946259eda7c0e8e
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-05-27 12:30:53 +0300

    all needed methods were implemented

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,26 @@
 package core.basesyntax;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
-    @Override
-    public String call() throws InterruptedException {
-        // write your code here
-        return null;
-    }
+    private SecureRandom random;
+
+    public MyThread() {
+            try {
+                this.random = SecureRandom.getInstanceStrong();
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException("Exception during initialization 'Random'.", e);
+            }
+        }
+
+        @Override
+        public String call() throws InterruptedException {
+            int durationTime = random.nextInt(200);
+            Thread.sleep(durationTime);
+            return "Task duration was " + random.nextInt(200)
+                    + " ms, execution finished at " + LocalTime.now();
+        }
 }
