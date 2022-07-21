208327d2d6fc125bc177aa99af1fbe9808eba0aa
karaedd <karaed.ml@mail.ru>
2022-07-21 10:56:55 +0300

    initial commit

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(100);
+        return "Task duration was 200 ms, " +
+                "execution finished at " + LocalTime.now();
     }
 }
