0333fc7b5d3f6693ecdc40c778164066694f202b
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-08-03 02:31:00 +0300

    Implement `Thread pool`

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,23 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final Random random;
+
+    public MyThread() {
+        this.random = new Random();
+    }
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int nextInt = random.nextInt(1000);
+        Thread.sleep(nextInt);
+        return "Task duration was " + nextInt
+                + " ms, execution finished at " + LocalTime.now()
+                .format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS"));
     }
 }
