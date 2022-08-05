20b860d712a08be118661f3491325c4b3e31e586
yaroslav <morozov.yrv@gmail.com>
2022-08-06 02:54:25 +0300

    ready

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import static java.time.temporal.ChronoUnit.MILLIS;
+
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime startTime = LocalTime.now();
+        Thread.sleep(new Random().nextInt(250) + 50);
+        LocalTime finishTime = LocalTime.now();
+        return String.format("Task duration was %d ms, execution finished at %s",
+                MILLIS.between(startTime, finishTime), finishTime);
     }
 }
