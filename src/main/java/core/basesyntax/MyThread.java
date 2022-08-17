5661bad56e6c79e52e8b29679732faf09da8b5ee
Mykola Yakymiv <mykola8426@gmail.com>
2022-08-17 14:40:56 +0300

    methods were implemented

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int timeSleep = new Random().nextInt(1000);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Thread.sleep(timeSleep);
+        return String.format("Task duration was %d ms, execution finished at %s",
+                timeSleep,
+                localDateTime.plusNanos(timeSleep).toString());
     }
 }
