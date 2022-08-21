00b80eb55145b3c9a593138042ab7de09dd59d80
oleg-new <9213195@gmail.com>
2022-08-21 22:36:14 +0300

    the static method newFixedThreadPool is implemented in the Main class, configured logging

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private int random;
+    private Random ran = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        random = (ran.nextInt(8) + 1) * 100;
+        Thread.sleep(random);
+        return "Task duration was " + random
+                + " ms, execution finished at " + LocalTime.now().toString();
     }
 }
