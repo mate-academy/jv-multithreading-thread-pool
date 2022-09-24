0eee10f99c5b5aa73174f1f0d97ef0c8f2275afd
drapail <mv.dibrov@gmail.com>
2022-09-24 19:56:02 +0300

    ExecutorService added

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +15,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        MyThread myThread = new MyThread();
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        for (int i = 0; i < 20; i++) {
+            Future<String> submit = executorService.submit(myThread);
+            futures.add(submit);
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.log(Level.INFO, future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
