6210daf92a7e5cb25ca2be32c6f10c3e5c6955c1
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-10-06 20:08:38 +0200

    Task conditions are done.

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -5,10 +5,10 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
-    private int duration;
-
     @Override
     public String call() throws InterruptedException {
+        int duration;
+
         Random random = new Random();
         duration = ((100 + random.nextInt(400)) / 100) * 100;
         Thread.sleep(duration);
