5e8eb3f2bda695b911161ca664aac0fab5f935db
Oleksii Mospan <alexey.mospan@gmail.com>
2022-10-17 22:18:39 +0300

    Initial commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +14,17 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            futures.add(threadPool.submit(new MyThread(new Random().nextInt(1000))));
+        }
+        threadPool.shutdown();
+        try {
+            for (Future<String> future : futures) {
+                logger.info(future.get());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Something went wrong", e);
+        }
     }
 }
