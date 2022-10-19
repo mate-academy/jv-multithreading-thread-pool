b0f1f2857169bf0dfdc3c97a431a6bac8dd9e696
Nazar <litvik.12@gmail.com>
2022-10-20 01:06:38 +0300

    used Callable

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        StringBuilder builder = new StringBuilder();
+        int randomMilliSec = new Random().nextInt(1000);
+        Thread.sleep(randomMilliSec);
+        builder.append("Task duration was ").append(randomMilliSec).append(" ms")
+                .append("execution finished at ").append(LocalDateTime.now());
+        return builder.toString();
     }
 }
