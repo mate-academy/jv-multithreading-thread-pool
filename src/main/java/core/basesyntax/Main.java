7eb9520f20a005a8dca86d5bff43e7fe241d48d9
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-10-24 16:28:04 +0300

    task is done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,33 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Integer NUMBER_OF_THREADS = 5;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
         // write your code here
+        MyThread myThread = new MyThread();
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        for (int i = 0; i < 20; i++) {
+            Future<String> returnResult = executorService.submit(myThread);
+            futures.add(returnResult);
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("Something went wrong!", e);
+            }
+        }
     }
 }
