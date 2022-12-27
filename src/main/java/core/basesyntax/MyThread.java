daf5694b30d17b63b55c15cafa0c5ce794d4b92b
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-12-27 19:59:33 +0200

    Thread pool solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int RANDOM_BOUND = 500;
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(100);
+        return "Task duration was " + new Random().nextInt(RANDOM_BOUND)
+                + " ms, execution finished at " + LocalTime.now();
     }
 }
