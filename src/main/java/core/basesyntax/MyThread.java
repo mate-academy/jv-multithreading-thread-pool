9cf7afa3686ed068140fb3aa7a3e41f54a320730
Maksym Shveda <mshveda28@gmail.com>
2022-12-29 22:37:16 +0200

    Implement Tasks executions

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
         // write your code here
-        return null;
+        long taskDuration = new Random().nextInt(200);
+        Thread.sleep(Math.abs(taskDuration));
+        return String.format("Task duration was %d ms,"
+                + " execution finished at %s", taskDuration, LocalTime.now());
     }
 }
