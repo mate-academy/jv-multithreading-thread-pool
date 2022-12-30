4ce965d286b4f350d890703e9a598b405ab22014
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-12-30 14:57:52 +0200

    implemented task

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long duration = ThreadLocalRandom.current().nextLong(200L);
+        Thread.sleep(duration);
+        LocalTime time = LocalTime.now();
+        return "Task duration was " + duration + " ms, execution finished at " + time.toString();
     }
 }
