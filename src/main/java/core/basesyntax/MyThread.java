bf78061f634f51e6acbf43c7d0d44accd26e456c
AlinaOryshak <alinaoryshak@gmail.com>
2022-12-30 18:46:41 +0200

    Realized thread pool

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int sleepPeriod = new Random().nextInt(300);
+        Thread.sleep(sleepPeriod);
+        return "Task duration was " + sleepPeriod + " ms, execution finished at " + LocalTime.now();
     }
 }
