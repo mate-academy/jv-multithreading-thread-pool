0393a53fc771b8f36a79ae26af6f67cd36a96053
Anastasiia Romanova <ana.roma.vi@gmail.com>
2023-01-01 18:17:43 +0200

    logged the result from each Future

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(200);
+        //System.out.println("My name is " + Thread.currentThread().getName());
+        return "Task duration was 200 ms, execution finished at " + LocalDateTime.now();
     }
 }
