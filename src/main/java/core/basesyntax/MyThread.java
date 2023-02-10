d3310cb954339f90c60383b24d5141fad9004501
Oleksandr Nesteruk <54321777tv@gmail.com>
2023-02-10 10:19:04 +0200

    Concurrent library practice

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int ms = new Random().nextInt(400);
+        Thread.sleep(ms);
+        return String.format("Task duration was %d ms, execution finished at %s",
+                ms, LocalTime.now());
     }
 }
