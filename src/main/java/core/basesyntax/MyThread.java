1dff08fcf268dd6d04a0a2b545b49f83a93d80c0
AndriySichniy <sichniy.andriy@gmail.com>
2023-02-22 11:04:40 +0200

    add logic

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,27 @@
 package core.basesyntax;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final Random random = new Random();
     @Override
     public String call() throws InterruptedException {
         // write your code here
-        return null;
+        Instant instantBegin = Instant.now();
+        long val = random.nextLong();
+        if (val < 0) {
+            val *= -1;
+        }
+        val %= 400;
+        val += 100;
+        Thread.sleep(val);
+        Instant instantEnd = Instant.now();
+        Duration duration = Duration.between(instantBegin, instantEnd);
+        return String.format("Task duration was %d ms, execution finished at %s",
+                duration.toMillis(), LocalDateTime.now());
     }
 }
