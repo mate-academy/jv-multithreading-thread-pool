40216e74e32d765f4cd440a89ce554d0b51164d9
Maxim Surzhik <kabaneri15@gmail.com>
2023-03-16 14:43:15 +0200

    done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +13,17 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 1; i <= 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
+        }
+        executorService.shutdown();
+        for (Future<String> f: futures) {
+            try {
+                logger.log(Level.INFO, f.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
