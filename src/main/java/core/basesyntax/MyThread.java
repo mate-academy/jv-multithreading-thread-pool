469398e539750f60c5968f25275f91cc215d17df
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2023-03-17 19:06:24 +0200

    Added implementation of call() and main() methods.

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        String result = "Task duration was " + new Random().nextInt(500) + "ms, "
+                + "execution finished at " + LocalTime.now();
+        return result;
     }
 }
