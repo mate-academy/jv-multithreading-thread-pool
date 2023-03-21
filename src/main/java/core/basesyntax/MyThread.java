c3a418afb6840b1b67770bd88cfba00761863359
Serhii Prykhodko <sergii.prykhodko@gmail.com>
2023-03-21 11:00:45 +0200

    implemented task

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Callable;
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        int duration = new Random().nextInt(200);
+        int duration = new Random().nextInt(500);
         Thread.sleep(duration);
         return "Task duration was " + duration + " ms, execution finished at " + LocalTime.now();
     }
