2db1b6b1652f05c28649511308c41a4b4297d657
Dmytro Vyshnivskyi <d.vyshnivskyi@gmail.com>
2023-03-28 13:55:02 +0300

    created thread pool

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +15,23 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            Future<String> submitedFuture = executorService.submit(new MyThread());
+            futures.add(submitedFuture);
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.MICROSECONDS);
+        } catch (InterruptedException e) {
+            logger.error("Executor service was interrupted!", e);
+        }
+        for (Future<String> future: futures) {
+            try {
+                logger.info(future.get());
+            } catch (ExecutionException | InterruptedException e) {
+                logger.error("Error getting result from Future", e);
+            }
+        }
     }
 }
