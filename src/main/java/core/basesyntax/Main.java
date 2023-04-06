d434aab68b4e9ece8b6938041007ab494ec06053
RomanPlk <roman.pylypchuk@yahoo.com>
2023-04-06 19:53:55 +0300

    removed unused import

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
