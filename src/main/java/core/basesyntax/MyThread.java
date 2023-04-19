5b38e65680a9786add387ec0a1ca6a04fd6e5d77
Roman Yuskevych <yuskevych@gmail.com>
2023-04-19 12:15:32 +0300

    added jv-multithreading-thread-pool homework solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int SLEEP_MULTIPLIER_BOUND = 7;
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int taskDuration = new Random().nextInt(SLEEP_MULTIPLIER_BOUND) * 100 + 100;
+        Thread.sleep(taskDuration);
+        System.out.println("Task duration was "
+                + taskDuration
+                + " ms, execution finished at "
+                + LocalTime.now());
+        return "Thread " + Thread.currentThread().getName() + " completed the job!";
     }
 }
