1add9e23dd36c0de61d9666267d7c4e7df4927ec
Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>
2023-05-12 11:25:06 +0300

    Update ci.yml to java 17

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,33 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int NUMBER_OF_THREADS = 5;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        Callable<String> task = new MyThread();
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get().toString());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("err", e);
+            }
+        }
+
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Double x = Math.random();
+        Long waiting = (long)(x * 300);
+        Thread.sleep(waiting);
+        LocalTime now = LocalTime.now();
+        return "Task duration was " + waiting + " ms, execution finished at " + now;
     }
 }
