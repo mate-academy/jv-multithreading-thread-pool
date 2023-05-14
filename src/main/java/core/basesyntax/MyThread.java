0a62bed3662fd900642dcc559db590149ad3c80f
Denys Dominskyi <dominskyj@gmail.com>
2023-05-14 20:55:38 +0300

    Initial commit

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final String MESSAGE = "Task duration was %2s ms, execution finished at %2s";
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        return String.format(MESSAGE,
+                new Random().nextInt(5) * 100 + 100, LocalTime.now());
     }
 }
