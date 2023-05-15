f8565faa5d77193858a7f60546b73f8a788b0942
Nazar Zvarych <zvarych45@gmail.com>
2023-05-15 16:50:57 +0300

    I do task, using ExecutorService.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +15,18 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> task = new MyThread();
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(task));
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException("Some exceptions here", e);
+            }
+        }
     }
 }
