0f64332778bb4bb3c2ba37c7ccc6a616e3d16a7c
kromchk <kromchyk@yahoo.com>
2023-05-15 09:27:17 +0300

    example of multithreading thread pool using

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +14,20 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Future<String>> futures = new ArrayList<Future<String>>();
+        Callable<String> callable = new MyThread();
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = executor.submit(callable);
+            futures.add(future);
+        }
+        for (Future<String> fut : futures) {
+            try {
+                System.out.println(fut.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
     }
 }
