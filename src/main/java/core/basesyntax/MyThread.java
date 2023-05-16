e895c44e6a14547104f5eb8ee5dd2ff57e68f62e
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-05-16 20:12:25 +0300

    Added Fixed thread pool, implemented MyThread

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        return new String("Task duration was "
+                + random.nextInt(500) + " ms, execution finished at "
+                + LocalDateTime.now());
     }
 }
