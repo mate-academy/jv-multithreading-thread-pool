6d42957ec65e2aad45972a8f62b131c58004aac4
Kyrylo Hyliuk <kyryl.huliuk@gmail.com>
2023-05-17 14:45:58 +0300

    Added hw solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     private static final String MESSAGE = "Task duration was %2s ms, execution finished at %2s";
+
     @Override
     public String call() throws InterruptedException {
         return MESSAGE.formatted(new Random().nextInt(5) * 100 + 100, LocalDateTime.now());
