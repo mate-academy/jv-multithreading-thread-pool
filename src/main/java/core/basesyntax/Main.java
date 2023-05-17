e6e26fc1a09cedb8fa9561e295e0d8d043f04179
Your Name <you@example.com>
2023-05-17 16:47:56 +0300

    implement the task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +14,17 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
+        }
+        executorService.shutdown();
+        for (Future<String> stringFuture : futures) {
+            try {
+                logger.info(stringFuture.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("Some exception was thrown", e);
+            }
+        }
     }
 }
