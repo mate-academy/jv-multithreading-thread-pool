786cd9996deef282447850c28a4ff4e423d803fd
Max Lutovinov <maxlutovinov@gmail.com>
2023-05-19 01:54:35 +0300

    Resolve Thread pool task

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long startTime = System.currentTimeMillis();
+        Thread.sleep(1000);
+        return String.format("Task duration was %d ms, execution finished at %s",
+                System.currentTimeMillis() - startTime, LocalTime.now());
     }
 }
