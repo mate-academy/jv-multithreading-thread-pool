b3021201bf5c4194b45f035f255dd4150e763421
Roman Usyk <roman.usik.93@ukr.net>
2023-05-19 13:24:32 +0300

    task was completed

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int timeSleeping = new Random().nextInt(1000);
+        Thread.sleep(timeSleeping);
+        return "Task duration was "
+                + timeSleeping
+                + " ms, execution finished at "
+                + LocalDateTime.now();
     }
 }
