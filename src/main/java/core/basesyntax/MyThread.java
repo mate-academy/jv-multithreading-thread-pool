91ed80e4ef7dd167b6726cf72c50cd009f880849
Mykyta Nikonets <nikita.nikonets@gmail.com>
2023-05-21 21:55:04 +0300

    implemented ExecutorService in main

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        int execTime = random.nextInt(500) + 100;
+        Thread.sleep(execTime);
+        return "Task execution was " + execTime + " ms, execution finished at " + LocalTime.now();
     }
 }
