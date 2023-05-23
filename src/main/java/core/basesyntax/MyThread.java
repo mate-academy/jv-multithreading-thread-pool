99174fae74a07e9d7f3c11f63e30da681b30165a
Oleksandr Krasnov <redman2775@gmail.com>
2023-05-23 07:42:26 +0300

    sol

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final String MESSAGE_FORMAT = "Task duration was %d ms, execution finished at %s";
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int sleepTime = new Random().nextInt(500);
+        Thread.sleep(sleepTime);
+        return String.format(MESSAGE_FORMAT, sleepTime, LocalDateTime.now());
     }
 }
