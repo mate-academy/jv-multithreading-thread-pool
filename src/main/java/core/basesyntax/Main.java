94753e25a4cd47bd5c055065a71b4bb9eaccc9ab
Oleksandr Krasnov <redman2775@gmail.com>
2023-05-23 07:44:54 +0300

    fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,7 +3,6 @@ package core.basesyntax;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -5,7 +5,9 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
-    private static final String MESSAGE_FORMAT = "Task duration was %d ms, execution finished at %s";
+    private static final String MESSAGE_FORMAT =
+            "Task duration was %d ms, execution finished at %s";
+
     @Override
     public String call() throws InterruptedException {
         int sleepTime = new Random().nextInt(500);
