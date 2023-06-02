d963bb0df5bf89f6166113c8ae0204966486baef
Serhii Shyian <serhii.shn@gmail.com>
2023-06-02 16:01:14 +0300

    1) In MyThread class added realization of call() method. 2) In Main class created threads, logged to file and printed to console.

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,21 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final Random random;
+
+    {
+        random = new Random();
+    }
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int val = (random.nextInt(5) + 1) * 100;
+        Thread.sleep(val);
+        return String.format("Task duration was %d ms, execution finished at %s",
+                val, LocalTime.now());
     }
 }
