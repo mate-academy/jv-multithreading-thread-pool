0c604469d17decebb687245d017829cac5383a36
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-09-13 01:04:56 +0300

    jv-multithreading-thread-pool

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +14,20 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        for (int i = 0; i < 20; i++) {
+            Callable<String> myThread = new MyThread();
+            Future<String> future = executorService.submit(myThread);
+            futures.add(future);
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
