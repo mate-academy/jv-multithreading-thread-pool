18b78cb072d810b322689ec2c73aa38fa2b6af34
Olexii Melnik <alexmelnikk@gmail.com>
2023-09-13 08:17:16 +0300

    fst commit

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int randomNumber = random.nextInt(5) + 1;
+        Thread.sleep(randomNumber * 100);
+        return "Task duration was " + randomNumber * 100
+                + " ms, execution finished at " + LocalTime.now();
     }
 }
