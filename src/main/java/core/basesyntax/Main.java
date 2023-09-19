07f6d3526e235926588ff7a87f685cce5d49b6b2
Denis Moroz <moroz06denis@gmail.com>
2023-09-19 13:59:28 +0200

    replace sout to loger

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,6 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,7 +23,7 @@ public class Main {
         executorService.shutdown();
         for (Future<String> future : futures) {
             try {
-                System.out.println(future.get());
+                logger.info(future.get());
             } catch (InterruptedException | ExecutionException e) {
                 throw new RuntimeException(e);
             }
