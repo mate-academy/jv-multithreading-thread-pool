469cf30a1effe1cd6d581c7e8d26508b75a4e0a5
Yura Khilko <yurakhilko@gmail.com>
2023-09-19 13:38:57 +0300

    added solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,5 +15,12 @@ public class Main {
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
         // write your code here
+        Callable<String> task = new MyThread();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(task));
+            //Future<String> future = executorService.submit(task);
+        }
+        executorService.shutdown();
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,24 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MyThread implements Callable<String> {
+    private static final Logger logger = LogManager.getLogger(MyThread.class);
+    private static Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
         // write your code here
-        return null;
+        long taskDuration = ThreadLocalRandom.current().nextLong(1000L);
+        Thread.sleep(taskDuration);
+        String log = String.format("Task duration was %s ms, execution finished at %s",
+                taskDuration, LocalTime.now());
+        logger.info(log);
+        return log;
     }
 }
