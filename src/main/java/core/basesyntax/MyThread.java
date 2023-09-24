e550818f13aee671f7e49506ec6cd04539c0c8de
Basil <champfockes@gmail.com>
2023-09-24 19:59:07 +0300

    corrections

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -8,8 +8,12 @@ public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
         Random rand = new Random();
-        int wait_time_ms = (rand.nextInt(5) + 1) * 100;
-        Thread.sleep(wait_time_ms);
-        return String.format("Task duration was %d ms, execution finished at %s", wait_time_ms, LocalTime.now());
+        int waitTimeMs = (rand.nextInt(5) + 1) * 100;
+        Thread.sleep(waitTimeMs);
+        return String.format(
+                "Task duration was %d ms, execution finished at %s",
+                waitTimeMs,
+                LocalTime.now()
+        );
     }
 }
