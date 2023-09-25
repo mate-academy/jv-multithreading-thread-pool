2b17460d2601ee6c3d519b729dd94274506628be
Yurii <wervmer>
2023-09-25 23:23:55 +0300

    in MyThread class implemented logic of call method

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final Random random = new Random();
+    private static final int RANDOM_BOUND = 10;
+    private static final String MESSAGE = "Task duration was %d00 ms, execution finished at ";
+    private static final DateTimeFormatter FORMAT =
+            DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS");
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        return String.format(MESSAGE, random.nextInt(RANDOM_BOUND))
+                + LocalTime.now().format(FORMAT);
     }
 }
