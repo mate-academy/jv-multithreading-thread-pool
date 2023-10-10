a873561be77e7b36d59040d0dbfc0f928a67b5cc
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-10 17:55:13 +0300

    fixed thread pool practice

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int randomTaskDuration = ((int) (Math.random() * 10) * 100);
+        Thread.sleep(randomTaskDuration);
+        return "Task duration was " + randomTaskDuration
+                + " ms, execution finished at " + LocalTime.now();
     }
 }
