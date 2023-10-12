c4c200576d5cbf9c688d843f60625e8b9023bd3c
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-10-12 19:15:10 +0300

    fixed checkstyle

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -5,8 +5,9 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
-    private final Random random = new Random();
     private static final int MAX_DURATION = 500;
+
+    private final Random random = new Random();
 
     @Override
     public String call() throws InterruptedException {
