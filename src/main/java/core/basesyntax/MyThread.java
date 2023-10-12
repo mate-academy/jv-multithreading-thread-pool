69f690cb503568e4e531316b4da67823e2ad18fb
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-10-12 19:10:24 +0300

    implemented multithreading-thread-pool

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final Random random = new Random();
+    private static final int MAX_DURATION = 500;
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int taskDuration = random.nextInt(MAX_DURATION);
+        Thread.sleep(taskDuration);
+        LocalTime finishTime = LocalTime.now();
+        return "Task duration was " + taskDuration + " ms, "
+                + "execution finished at " + finishTime;
     }
 }
