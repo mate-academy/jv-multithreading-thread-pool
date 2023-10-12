4baf1c16c13d890bd3bcceeeb3d1b91b35202edc
Oleksandr Hatsenko <a.s.gatsenko@gmail.com>
2023-10-12 16:27:30 +0300

    Homework solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private Random rnd = new Random();
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int taskDuration = rnd.nextInt(600);
+        Thread.sleep(taskDuration);
+        return "Task duration was " + taskDuration + " ms, "
+                + "execution finished at " + LocalTime.now();
     }
 }
