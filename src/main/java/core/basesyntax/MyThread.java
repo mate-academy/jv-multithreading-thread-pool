41fb04bb235aa3171e044acc6738e3c582c2e418
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-12 13:39:44 +0300

    Impl executor sevice

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int MAX_DURATION = 501;
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int taskDuration = new Random().nextInt(MAX_DURATION);
+        Thread.sleep(taskDuration);
+        LocalTime executionTime = LocalTime.now();
+        return "Task duration was " + taskDuration + " ms, execution finished at " + executionTime;
     }
 }
