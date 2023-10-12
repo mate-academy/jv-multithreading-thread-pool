a1e04f381b7d8ec9147c236f9244164d5873b6e4
Oleksandr Savenets <134528120+OleksandrSaven@users.noreply.github.com>
2023-10-12 12:29:30 +0300

    Add constant

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -12,11 +12,12 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int NUMBER_OF_TREADS = 5;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
         
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_TREADS);
         Callable<String> task = new MyThread();
         for (int i = 0; i < 20; i++) {
             futures.add(executor.submit(task));
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -5,9 +5,13 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int MILLS = 100;
+    private static final int BOUND = 6;
+    private static final int COEFFICIENT = 1;
+
     @Override
     public String call() throws InterruptedException {
-        int duration = Math.abs((new Random().nextInt(6) + 1) * 100);
+        int duration = Math.abs((new Random().nextInt(BOUND) + COEFFICIENT) * MILLS);
         Thread.sleep(duration);
         return "Task duration was " + duration
                 + "ms" + ", execution finished at " + LocalTime.now();
