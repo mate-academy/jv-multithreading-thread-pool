a442e6b24077176c85c922920c455fbcce2ea1a1
Oleksandr Hatsenko <a.s.gatsenko@gmail.com>
2023-10-12 16:30:37 +0300

    Fixed style

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     private Random rnd = new Random();
+
     @Override
     public String call() throws InterruptedException {
         int taskDuration = rnd.nextInt(600);
