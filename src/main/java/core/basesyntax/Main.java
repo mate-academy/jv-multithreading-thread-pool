8648e201a8fefb6b30a9315953f6ea61d661fb2b
Artem Hlukhoman <giantking1231@gmail.com>
2023-10-28 15:34:17 +0300

    hw is done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,32 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int THREADS_NUMBER = 5;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADS_NUMBER);
+        Callable<String> task = new MyThread();
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(task));
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("Error", e);
+            }
+        }
     }
 }
