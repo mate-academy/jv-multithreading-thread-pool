b8d1f8b62d7a33fa86b9b3b9e1b04d111a396a56
Melnychuk <melnychukmyhailo@gmail.com>
2023-10-31 10:03:51 +0200

    MyThread logic was added

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = new Random().nextInt(5) * 100 + 100;
+        Thread.sleep(duration);
+        return Thread.currentThread().getName()
+                + " Task duration was " + duration
+                + "ms, execution finished at " + LocalTime.now();
     }
 }
