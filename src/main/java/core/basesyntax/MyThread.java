41c773af416f53c7004a745e051bf09a3e472694
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-12-06 15:30:03 +0200

    implemented thread-pool

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long start = System.currentTimeMillis();
+        Thread.sleep((random.nextInt(5) + 1) * 100);
+        LocalTime endTime = LocalTime.now();
+        long end = System.currentTimeMillis();
+        long duration = (end - start);
+        return String.format("Task duration was %s ms, execution finished at %s",
+                duration, endTime);
     }
 }
