7c7f36d0917a73be2671979f60f5bb7f0bf8751a
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-12-13 09:37:38 +0200

    implemented methods

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +15,23 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = executorService.submit(new MyThread());
+            futures.add(future);
+        }
+
+        for (Future<String> future : futures) {
+            try {
+                String result = future.get();
+                logger.info(result);
+            } catch (InterruptedException | ExecutionException e) {
+                logger.error("Exception occurred: " + e.getMessage());
+            }
+        }
+
+        executorService.shutdown();
     }
 }
