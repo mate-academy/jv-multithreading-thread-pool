213789f734f1fe399face62954cffc56a6da733d
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-12-28 14:52:28 +0200

    added implementation for threadPool task and logged output in ci.yml file

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,34 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final String EXCEPTION = "Something went wrong";
+    private static final Integer START = 0;
+    private static final Integer END = 20;
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> callable = new MyThread();
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        for (int i = START; i <= END; i++) {
+            futures.add(executorService.submit(callable));
+        }
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(EXCEPTION);
+            }
+        }
     }
 }
