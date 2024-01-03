611d2e8d95ec8070f1d984ca6ed30a8dd7a7a608
denysSmeiNewBom <den.smei.ds@gmail.com>
2024-01-03 22:16:35 +0200

    Task completed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,16 +1,31 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> myThread = new MyThread();
+        int i = 0;
+        while (i < 20) {
+            futures.add(executorService.submit(myThread));
+            i++;
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException("Some exception occurred:/");
+            }
+        }
     }
 }
