a1e4e8223d8d7a99fcc19c90abeaca5ec26ac4b6
Anton Babych <antonbabi13@gmail.com>
2024-01-12 10:53:59 +0200

    done task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,7 +14,22 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        try {
+            List<Future<String>> futures = new ArrayList<>();
+            Callable<String> callable = new MyThread();
+            for (int i = 0; i < 20; i++) {
+                Future<String> future = executorService.submit(callable);
+                futures.add(future);
+            }
+            for (Future<String> future : futures) {
+                String value = future.get();
+                logger.info(value);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executorService.shutdown();
+        }
     }
 }
