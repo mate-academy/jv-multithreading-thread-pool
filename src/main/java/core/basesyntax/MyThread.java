66b1987dab00f51b4f9d6cc610fd062890e577f3
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-25 21:44:48 +0200

    Fixed checkstyle

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -6,11 +6,12 @@ import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        return "Task duration was " +
-                random.nextInt(500) +
-                " ms, execution finished at " +
-                LocalTime.now();
+        return "Task duration was "
+                + random.nextInt(500)
+                + " ms, execution finished at "
+                + LocalTime.now();
     }
 }
