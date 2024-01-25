c5a234909041af956351ad34a2d7d02db0db7a11
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-25 23:02:52 +0200

    Added task impl.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,35 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int POOL_SIZE = 5;
+    private static final int NUMBER_OF_THREADS = 20;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
+            Callable<String> callable = new MyThread();
+            for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+                Future<String> future = executorService.submit(callable);
+                futures.add(future);
+            }
+
+            for (Future<String> future : futures) {
+                logger.info(future.get());
+            }
+            executorService.shutdown();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int TOP_BOUND = 500;
+    private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int currentMills = random.nextInt(TOP_BOUND);
+        Thread.sleep(currentMills);
+        StringBuilder builder = new StringBuilder();
+        builder.append("Task duration was ")
+                .append(currentMills)
+                .append(" ms, execution finished at ")
+                .append(LocalDateTime.now());
+        return builder.toString();
     }
 }
