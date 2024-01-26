76c26405e9fdcb024b83503c734f43d1b73ed2ce
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-01-26 16:57:28 +0200

    main method and MyThread class realized

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,31 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final int THREADS_QUANTITY = 20;
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < THREADS_QUANTITY; i++) {
+            Future<String> future = executorService.submit(new MyThread());
+            futures.add(future);
+        }
+        executorService.shutdown();
+        try {
+            for (Future<String> futureFromList : futures) {
+                logger.info(futureFromList.get());
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
