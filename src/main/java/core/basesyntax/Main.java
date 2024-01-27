dac61f0678207a674217e61d5a0e7e4b10b3c545
Lenovo <liubchenkalexander@gmail.com>
2024-01-27 20:55:03 +0200

    done the task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +13,21 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 20; i++) {
+            MyThread myThread = new MyThread();
+            futures.add(executorService.submit(myThread));
+        }
+        executorService.shutdown();
+
+        for (Future<String> future : futures) {
+            try {
+                String result = future.get();
+                System.out.println(result);
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        int randomTime = random.nextInt(500);
+        Thread.sleep(randomTime);
+        return "Task duration was " + randomTime
+                + " ms, execution finished at " + LocalTime.now();
     }
 }
