e30c684ee51a2537edb62f7e473a1929792e6e57
artificialLeo <horkovenko.k@gmail.com>
2024-02-13 18:47:44 +0200

    init

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,48 @@
 package core.basesyntax;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MyThread implements Callable<String> {
+    private static final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int taskDuration = random.nextInt(500) + 100;
+        Thread.sleep(taskDuration);
+        LocalTime executionTime = LocalTime.now();
+        return "Task duration was " + taskDuration + " ms, execution finished at " + executionTime;
+    }
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(Main.class);
+        List<Future<String>> futures = new ArrayList<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = executorService.submit(new MyThread());
+            futures.add(future);
+        }
+
+        executorService.shutdown();
+
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (Exception e) {
+                logger.error("Error retrieving result from Future", e);
+            }
+        }
     }
 }
+
