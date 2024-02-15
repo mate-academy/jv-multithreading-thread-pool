7b1e66e762e69f1ca7fd2fbcb7342798b4a893f3
Sonia Baranova <sofiasapsan@gmail.com>
2024-02-15 11:52:31 +0200

    added solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private Random random = new Random();
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long min = 100; // Мінімальне значення діапазону
+        long max = 600; // Максимальне значення діапазону (не включно)
+        long randomNumber = min + (long)(random.nextDouble() * (max - min));
+        Thread.sleep(randomNumber);
+        return String.format("INFO core.basesyntax.Main:24" +
+                " Task duration was %d ms, execution finished at %s",
+                randomNumber, LocalTime.now());
     }
 }
