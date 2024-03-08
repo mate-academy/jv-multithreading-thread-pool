dd85e54104a69c25daebc0e62a93c78717a3bd67
Sergey Shestaka <sergey.shestaka@gmail.com>
2024-03-08 14:33:32 +0100

    solution-1

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int randomDuration = (new Random().nextInt(9) + 1) * 100;
+        Thread.sleep(randomDuration);
+        return String.format("Task duration was %d ms, execution finished at %tT.%tN",
+                randomDuration,
+                LocalTime.now(),
+                LocalTime.now());
     }
 }
