166cd5aaeb0f6f1306fcbdd7971e7143d6ca25ae
Dmytro Hanshyn <di.hanshyn@gmail.com>
2024-03-12 19:36:25 +0200

    thread-pool

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,28 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> task = new MyThread();
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(task));
+        }
+
+        executorService.shutdown();
+
+        for (Future<String> future: futures) {
+            System.out.println(future.get());
+            logger.info(future.get());
+        }
     }
 }
