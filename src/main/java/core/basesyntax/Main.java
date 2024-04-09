07d27adb2ae85515aa832a3489cc9dac690256a2
Vlad Bondarev <bondarev.21@icloud.com>
2024-04-09 10:49:44 +0300

    hw

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +14,21 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
+            MyThread myThread = new MyThread();
+            for (int i = 0; i < 20; i++) {
+                Future<String> future = executorService.submit(myThread);
+                futures.add(future);
+            }
+            executorService.shutdown();
+            for (Future<String> future : futures) {
+                String message = future.get();
+                logger.info(message);
+                System.out.println(message);
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,25 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime before = LocalTime.now();
+        long nanoOfDayBefore = before.toNanoOfDay();
+        int sleep = new Random().nextInt(500);
+        Thread.sleep(sleep);
+
+        long nanoOfDayAfter = LocalTime.now().toNanoOfDay();
+
+        long difference = nanoOfDayAfter - nanoOfDayBefore;
+
+        return String.format(
+                "Task duration was %s ms, execution finished at %s",
+                difference / 1_000_000,
+                before
+        );
     }
 }
