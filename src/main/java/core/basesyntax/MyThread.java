d6e2ee7103a832ea0ea0961377f893d72217d643
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-04-11 07:47:32 +0300

    MyThread.java

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        int duration = 100 + random.nextInt(400);
+        Thread.sleep(duration);
+        String finishTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSS"));
+        return String.format("Task duration was %d ms, execution finished at %s", duration, finishTime);
     }
 }
