3b28340e13ce30fe7df2595fe1894256ccebb98a
Yevhenii Liashenko <paradoxrial@gmail.com>
2024-04-12 14:25:57 +0300

    make task

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private int duration;
+
+    public MyThread(int duration) {
+        this.duration = duration;
+    }
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime startTime = LocalTime.now();
+        Thread.sleep(duration);
+        LocalTime endTime = LocalTime.now();
+        return "Task duration was " + duration + " ms, execution finished at " + endTime + ".";
     }
 }
