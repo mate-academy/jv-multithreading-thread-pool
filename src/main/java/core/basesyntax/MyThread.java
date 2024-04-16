b8893f58ac9a2c47a1737bfc4892db6638c335b8
Ostap Danyliv <danyliv.ostap@gmail.com>
2024-04-16 15:45:36 -0500

    Executor service solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+
+    private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int delay = random.nextInt(11) * 100;
+        Thread.sleep(delay);
+        return String.format("Task duration was %s ms, execution finished at %s",
+            delay, LocalTime.now());
     }
 }
