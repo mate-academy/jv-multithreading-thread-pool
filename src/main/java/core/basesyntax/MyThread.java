28b30d6ad220b3dfb7dc096bb6b9084c8ea54dd5
Wasja Zejkan <vasazejkan5@gmail.com>
2024-04-19 18:02:11 +0300

    Created ExecutorService instance in main() method, implemented call() method in class MyThread

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
         // write your code here
-        return null;
+        long duration = new Random().nextInt(500);
+        Thread.sleep(duration);
+        return String.format("Task duration was %s ms, execution finished at %s",
+                duration, LocalTime.now());
     }
 }
