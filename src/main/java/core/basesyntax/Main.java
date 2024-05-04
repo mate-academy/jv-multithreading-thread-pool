f7c94554795945f062582a784544ae1fe784e321
Pavlo Polezhay <pasha200417@gmail.com>
2024-05-04 10:04:50 +0300

    complete task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,24 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
+        }
+        for (Future<String> future : futures) {
+            logger.info(future.get());
+        }
+        executorService.shutdown();
     }
 }
