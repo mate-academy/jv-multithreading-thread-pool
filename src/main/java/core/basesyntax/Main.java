e373c4b69527ffaeab419723166e9bb28b9d8b5d
yulka-tsybulka <kasinets.ulia@gmail.com>
2024-05-28 13:17:23 +0300

    fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
