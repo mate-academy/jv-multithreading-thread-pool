dc5a4d3d6791c8c7e0599ae0d6ca818953973339
sasha <radovietsoleksandr@gmail.com>
2024-05-29 01:12:35 +0300

    q1

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,12 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +16,34 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 20; i++) {
+            MyThread myThread = new MyThread((new Random().nextInt(1000) + 100));
+            Future<String> future = executorService.submit(myThread);
+            futures.add(future);
+        }
+
+        for (Future<String> future : futures) {
+            try {
+                String result = future.get();
+                logger.info(result);
+            } catch (InterruptedException | ExecutionException e) {
+                logger.error("Task execution failed", e);
+            }
+        }
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
+                logger.warn("Executor did not terminate in the specified time.");
+                List<Runnable> droppedTasks = executorService.shutdownNow();
+                logger.warn("Executor was abruptly shut down. {} tasks will not be executed.",
+                        droppedTasks.size());
+            }
+        } catch (InterruptedException e) {
+            logger.error("Termination interrupted", e);
+            executorService.shutdownNow(); // Форсированное завершение при ошибке
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final int duration;
+
+    public MyThread(int duration) {
+        this.duration = duration;
+    }
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(duration);
+        LocalTime finishTime = LocalTime.now();
+        return String.format(
+                "Task duration was %d ms, execution finished at %s",
+                duration,
+                finishTime);
     }
 }
