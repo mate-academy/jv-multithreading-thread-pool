30f6631af5cf3a6941064c8183401396cc624ab0
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-06-19 18:30:10 +0300

    solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime start = LocalTime.now();
+        Thread.sleep(100);
+        LocalTime end = LocalTime.now();
+        Duration duration = Duration.between(start, end);
+        long millis = duration.toMillis();
+        return String.format("Task duration was %d ms, execution finished at ", millis)
+                    + LocalTime.now();
     }
 }
