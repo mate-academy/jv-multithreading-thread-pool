b5ef704d6cb064ed18ef476c3c3b1fa3f71f8410
Valentyn Sharshon <woosay333@gmail.com>
2024-06-19 19:16:01 +0300

    Thread pool implementation

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,31 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int THREAD_POOL_SIZE = 5;
+    private static final int TASK_AMOUNT = 20;
 
-    public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        try (ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE)) {
+            List<MyThread> tasks = new ArrayList<>();
+            for (int i = 0; i < TASK_AMOUNT; i++) {
+                tasks.add(new MyThread());
+            }
+
+            List<Future<String>> futures = executorService.invokeAll(tasks);
+            for (Future<String> task : futures) {
+                logger.info(task.get());
+            }
+
+            executorService.shutdown();
+        }
     }
 }
