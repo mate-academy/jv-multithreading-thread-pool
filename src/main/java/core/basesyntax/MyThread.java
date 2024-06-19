89dcc709759bf01a14d7ec744569bdb569d8c150
Pavlo Betsa <bigworld2303@gmail.com>
2024-06-19 13:26:27 +0300

    Style fixed

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -6,10 +6,14 @@ import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     private static final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
         int randomDuration = random.nextInt(500) + 100;
         Thread.sleep(randomDuration);
-        return "Task duration was " + randomDuration + " execution finished at " + LocalDateTime.now();
+        return "Task duration was "
+                + randomDuration
+                + " execution finished at "
+                + LocalDateTime.now();
     }
 }
