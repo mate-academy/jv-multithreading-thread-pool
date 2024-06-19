7a422953d8aa46eeef794f971ce1108c26ffd540
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-06-20 01:54:20 +0300

    feat : initial commit of a task

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalTime startTime = LocalTime.now();
+        Thread.sleep(new Random().nextInt(1000));
+        LocalTime endTime = LocalTime.now();
+        long duration = Duration.between(startTime, endTime).toMillis();
+        return String.format("Task duration was %d ms, execution finished at %s", duration, endTime);
     }
 }
