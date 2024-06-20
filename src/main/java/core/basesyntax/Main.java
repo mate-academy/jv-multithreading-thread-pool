450f8d9143b33dc3285050fd91dab1e6a6db28c4
bodya4243 <bogusbogdan67@gmail.com>
2024-06-20 16:05:21 +0300

    1) the task is done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,8 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +13,24 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 20; i++) {
+            int taskDuration = 200;
+            MyThread myThread = new MyThread(taskDuration);
+            Future<String> future = executor.submit(myThread);
+            futures.add(future);
+        }
+
+        executor.shutdown();
+
+        for (Future<String> future : futures) {
+            try {
+                String result = future.get();
+                logger.info(result);
+            } catch (Exception e) {
+                logger.error("Error while getting the result from future", e);
+            }
+        }
     }
 }
