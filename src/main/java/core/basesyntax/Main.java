a715254400958f8d18ec77d91d06a00f241a8352
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-26 19:10:50 +0300

    added solution to the task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +15,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Callable<String>> threadList = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            threadList.add(new MyThread());
+        }
+        try {
+            futures = executor.invokeAll(threadList);
+            executor.shutdown();
+            for (Future<String> future : futures) {
+                logger.info(future.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException("Encountered with exception: ", e);
+        }
     }
 }
