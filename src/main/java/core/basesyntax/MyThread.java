8418ee33284d7f8b6a9bfbcedaf02a79c4018806
Olesia Kapustynska <d.olleska@gmail.com>
2024-06-28 21:50:20 +0300

    add solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final LocalDateTime time = LocalDateTime.now();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        float duration = (float) (LocalDateTime.now().getNano() - time.getNano()) / 1000000;
+        return "Task duration was " + duration + " ms, execution finished at "
+                + LocalDateTime.now();
     }
 }
