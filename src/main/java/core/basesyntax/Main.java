833f2cd9b77a02f50aba44e2794ff674c3ff358b
AshaJaneBrown <ashajanebrown@gmail.com>
2024-07-13 12:23:32 +0300

    fix added

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -14,21 +14,20 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        try (ExecutorService executorService = Executors.newFixedThreadPool(5)) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-            for (int i = 0; i < 20; i++) {
-                Future<String> future = executorService.submit(new MyThread());
-                futures.add(future);
-            }
-            executorService.shutdown();
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = executorService.submit(new MyThread());
+            futures.add(future);
+        }
+        executorService.shutdown();
 
-            try {
-                if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
-                    executorService.shutdownNow();
-                }
-            } catch (InterruptedException e) {
+        try {
+            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
                 executorService.shutdownNow();
             }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
         }
 
         for (Future<String> future : futures) {
