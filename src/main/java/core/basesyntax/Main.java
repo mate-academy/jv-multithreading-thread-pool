f9ad5c97662813e8fcd387cc6c8021e378815fce
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-09-01 12:05:44 +0200

    solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,8 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +11,24 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<String> myThread = new MyThread();
+
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = executorService.submit(myThread);
+            futures.add(future);
+        }
+
+        for (Future<String> future : futures) {
+            try {
+                String value = future.get();
+                System.out.println(value);
+            } catch (InterruptedException | ExecutionException e) {
+                logger.error("An error occurred while executing the task.", e);
+            }
+        }
+        executorService.shutdown();
     }
 }
