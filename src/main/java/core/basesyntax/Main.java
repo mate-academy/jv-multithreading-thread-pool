7c7bf208397e301a2501be2995a10f649393670b
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-09-13 05:42:34 +0300

    solved

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,10 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +13,28 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        for (int i = 0; i < 20; i++) {
+            MyThread task = new MyThread();
+            Future<String> future = executorService.submit(task);
+            futures.add(future);
+        }
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+        for (Future<String> future : futures) {
+            try {
+                String result = future.get();
+                logger.info(result);
+            } catch (Exception e) {
+                logger.error("Error while getting future result", e);
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int taskDuration = ThreadLocalRandom.current().nextInt(100, 501);
+        Thread.sleep(taskDuration);
+        LocalTime finishTime = LocalTime.now();
+        return String.format("Task duration was "
+                + "%d ms, execution finished at %s", taskDuration, finishTime);
+
     }
 }
