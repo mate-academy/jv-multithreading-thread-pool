a0019caf530d8bf3a56c4bc7fb32ee4a560ceca1
Sergei <chertterner686@gmail.com>
2024-09-22 20:15:27 +0300

    first

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,12 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
         // write your code here
-        return null;
+        return "Task duration was 200 ms, execution finished at " + LocalTime.now();
     }
 }
