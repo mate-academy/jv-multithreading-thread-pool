4fa311203c370ea754b20505dfc9a7afaf87abdc
IvanovYurii <ivanovyura76@gmail.com>
2024-09-30 21:05:19 +0300

    Solution of this task

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private final int duration;
+
+    public MyThread(int duration) {
+        this.duration = duration;
+    }
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(duration);
+        return "Task duration was " + duration + " ms, execution finished at " + LocalTime.now();
     }
 }
