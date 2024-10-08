47bf78e25a7f0eae0c7b49e6b833100744b7deee
Watxesnavo <112902418+Watxesnavo@users.noreply.github.com>
2024-10-08 14:38:07 +0200

    1st commit: task done

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MyThread implements Callable<String> {
     @Override
-    public String call() throws InterruptedException {
-        // write your code here
-        return null;
+    public String call() {
+        int taskDuration = ThreadLocalRandom.current().nextInt(10, 1000);
+        LocalTime endTime = LocalTime.now();
+        return String.format("Task duration was %d ms, execution finished at %s", taskDuration, endTime);
     }
 }
