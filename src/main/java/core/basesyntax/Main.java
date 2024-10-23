83b5ce60ea508ddf9a9232e1d84dfc7d34c7d047
Artem Klishch <artemklishch@gmail.com>
2024-10-23 20:52:32 +0300

    feat: should add threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,6 +14,19 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        MyThread myThread = new MyThread();
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = executorService.submit(myThread);
+            futures.add(future);
+        }
+        executorService.shutdown();
+        for (Future<String> future : futures) {
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
