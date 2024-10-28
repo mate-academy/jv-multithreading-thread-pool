515f38e77a38510e0aa5bd7309ff3ac5c6d1e6bd
Nikita <niklafinskiy@gmail.com>
2024-10-28 17:32:03 +0100

    implemented the async thread handling

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,30 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int THREADS_NUMBER = 5;
+    private static final int NUMBER_OF_EXECUTIONS = 20;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        try (ExecutorService executorService = Executors.newFixedThreadPool(THREADS_NUMBER)) {
+            for (int i = 0; i < NUMBER_OF_EXECUTIONS; i++) {
+                futures.add(executorService.submit(new MyThread()));
+            }
+
+            for (Future<String> future : futures) {
+                logger.info(future.get());
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException("Error while executing threads", e);
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,18 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int RANDOM_BOUND = 1000;
+    private final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(random.nextInt(RANDOM_BOUND));
+        return String.format("Task duration was %s ms, execution finished at %s%n",
+                RANDOM_BOUND,
+                LocalTime.now());
     }
 }
