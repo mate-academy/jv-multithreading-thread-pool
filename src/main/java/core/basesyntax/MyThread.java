84f603d6255d957a0c35bf1c6bcdd3af48ade380
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-12-13 18:54:01 +0200

    jv-multithreading-thread-pool solution

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = new Random().nextInt(5);
+        duration = (duration + 1) * 100;
+        Thread.sleep(duration);
+        return String.format(
+                "Task duration was %d ms, execution finished at %s", duration, LocalDateTime.now());
     }
 }
