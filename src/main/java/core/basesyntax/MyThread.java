bf82bef85364ab667c662719d4f31f6f36f48927
iurii <kenu21@gmail.com>
2024-12-31 19:29:46 +0100

    EXECUTOR: added executor

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final Random RANDOM = new Random(47);
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        LocalDateTime start = LocalDateTime.now();
+        Thread.sleep(RANDOM.nextInt(1000));
+        LocalDateTime finish = LocalDateTime.now();
+        long duration = Duration.between(start, finish).toMillis();
+        return String.format("Task duration was %s ms, execution finished at %s", duration, finish);
     }
 }
