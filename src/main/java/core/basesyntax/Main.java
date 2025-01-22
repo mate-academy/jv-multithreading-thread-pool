19b951d4f34623341e26ea5b463bef34eb7c59b8
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2025-01-22 23:44:51 +0200

    Solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,9 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,8 +12,23 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 1; i <= 20; i++) {
+            futures.add(executorService.submit(new MyThread()));
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (Future<String> future : futures) {
+            builder.append(future.get()).append(System.lineSeparator());
+        }
+
+        logger.info(builder.toString());
+
+        executorService.shutdown();
     }
 }
