95ce0446fdf9479c6a9f3616532d30c2eb645537
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2025-02-10 13:30:22 +0100

    Done Thread pool. Attempt 1.0 - realized method in MyThread; - tested in main method;

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = new Random().nextInt(401) + 100;
+        Thread.sleep(duration);
+        LocalTime finishTime = LocalTime.now();
+        return "Task duration was " + duration + " ms, execution finished at " + finishTime;
     }
 }
