ad30eb21b1d24576a6f53b126fcb66669a9474e5
mrmax24 <hrytsynamax@gmail.com>
2025-03-05 16:05:06 +0200

    logging futures execution in thread pool implemented

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,20 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        int duration = 100 + random.nextInt(401);
+
+        Thread.sleep(duration);
+
+        LocalTime finishTime = LocalTime.now();
+
+        return String.format("Task duration was %d ms, "
+                + "execution finished at %s", duration, finishTime);
     }
 }
