e3988ef75c90fee9d51af6330f2a103ea7d0a5d3
Kate892F <tophours13@gmail.com>
2025-03-19 10:50:18 +0200

    task done

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final int TASK_DURATION = 200;
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Thread.sleep(TASK_DURATION);
+
+        return String.format("Task duration was %s ms,  execution finished at %s",
+                TASK_DURATION, LocalTime.now());
+
     }
 }
