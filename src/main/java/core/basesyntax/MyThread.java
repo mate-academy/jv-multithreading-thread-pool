85d4cf728875334ea4709aa691d146e1c6fe7ca1
Igor Neminuschii <igorgorslav@gmail.com>
2025-03-21 12:56:53 +0200

    created an ExecutorService instance

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,14 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        int duration = random.nextInt(500);
+        return "Task duration was " + duration + " ms, execution finished at " + LocalTime.now();
     }
 }
