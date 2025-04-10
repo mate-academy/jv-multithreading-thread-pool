e097a066cf106cc6f1c92694dee4c6129a185668
VArabchuk <arabon3@gmail.com>
2025-04-10 15:52:06 +0300

    Add fixed thread pool execution and log output to file

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long startTime = System.nanoTime();
+
+        Thread.sleep(200);
+
+        long endTime = System.nanoTime();
+
+        long durationInMillis = (endTime - startTime) / 1_000_000; // Перетворення на мілісекунди
+
+        return String.format(
+                "Task duration was %d ms, execution finished at %s",
+                durationInMillis, LocalTime.now()
+        );
     }
 }
