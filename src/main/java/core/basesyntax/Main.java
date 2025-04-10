fdbe1e238b308e93c744e65798f20099165af63a
JaverJaver <mopasaj205@arpizol.com>
2025-04-11 00:11:18 +0300

    pool executor

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,31 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int threads = 5;
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(threads);
+        for (int i = 0; i < 20; i++) {
+            Future<String> future = threadPoolExecutor.submit(new MyThread());
+            futures.add(future);
+        }
+        threadPoolExecutor.shutdown();
+        futures.forEach((future) -> {
+            try {
+                logger.info(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long start = System.currentTimeMillis();
+        Thread.sleep(300L);
+        long end = System.currentTimeMillis();
+
+        return String.format("Task duration was %d ms, execution finished at %s",
+                end - start, LocalDateTime.now());
     }
 }
