0b1b344a77f85a96766bc814e53f37eae30676f7
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-04-25 17:03:51 +0200

    Second solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        // write your code
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         for (int i = 0; i < 20; i++) {
             futures.add(executorService.submit(new MyThread()));
