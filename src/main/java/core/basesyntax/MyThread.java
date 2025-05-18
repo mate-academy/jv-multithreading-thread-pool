14476d4928ab1ee83786c52c45c99ac110f285ef
StoneBlood <brkalix@gmail.com>
2025-05-18 15:13:03 +0300

    implemented the method call()

--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,25 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final Random random = new Random();
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int duration = 250 + random.nextInt(140);
+        try {
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return "The Task was interrupted";
+        }
+        LocalDateTime finishedTime = LocalDateTime.now();
+        return String.format(
+                "Task duration was %d ms, execution finished at %s",
+                duration, finishedTime
+        );
     }
 }
