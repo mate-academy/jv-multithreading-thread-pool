556086dc716aee42600b9ac36b8dd55d5898c85f
Denys Mazurenko <0gur4ik3992@gmail.com>
2025-05-22 23:46:32 +0300

    solved task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,15 +2,30 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final int THREAD_COUNT = 5;
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Future<String> future = executorService.submit(new MyThread());
+            futures.add(future);
+        }
+        for (Future<String> future : futures) {
+            try {
+                logger.info(future.get());
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+        executorService.shutdown();
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,16 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        long start = System.currentTimeMillis();
+        Thread.sleep(2000);
+        return "Task duration was "
+                + (System.currentTimeMillis() - start)
+                + " ms , execution finished"
+                + " at " + LocalDateTime.now();
     }
 }
